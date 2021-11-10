9079f1c4722f4b9df3331ba87307774d60a32e5f
yujin6378 <93787702+yujin6378@users.noreply.github.com>
2021-11-10 21:02:08 +0900

    -User, UserRepository 추가

---
+++ src/main/java/com/j3dev/ims/springboot/web/api/user/User.java
@@ -0,0 +1,13 @@
+package com.j3dev.ims.springboot.web.api.user;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Entity
+@Table(name = "USER")
+public class User {
+
+}
---
+++ src/main/java/com/j3dev/ims/springboot/web/api/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.j3dev.ims.springboot.web.api.user;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+}
